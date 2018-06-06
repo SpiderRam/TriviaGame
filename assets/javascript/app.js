a3e01b00214664e9030c11f50049dfc8c0cfe031
Carmen Owen <carmenowen@carmenocomputer.home>
2018-06-06 16:34:25 -0400

    mostly there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -64,29 +64,123 @@ const questions = [
 
 
 var counter = 120;
+var numberCorrect = 0;
+var numberWrong = 0;
+var numberUnanswered = 0;
 
 var timer; 
 
-function getAnswers () {
-    let answers = [];
-    $("input:radio").each(function () {
-    var $this = $(this),
-        id = $this.attr('id');
+function getAnswers() {
+   //try a for loop, with question-[i]
+    $.each($("input[name='question-0']:checked"), function(){
+        if($(this).val() === questions[0].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
+    })
+    $.each($("input[name='question-1']:checked"), function(){
+        if($(this).val() === questions[1].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
+    });
+    $.each($("input[name='question-2']:checked"), function(){
+        if($(this).val() === questions[2].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
+    });
+    $.each($("input[name='question-3']:checked"), function(){
+        if($(this).val() === questions[3].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
 
-       if ($(this).prop(':checked')) {
-            answers.push(this);
-       }
+    });
+    $.each($("input[name='question-4']:checked"), function(){
+        if($(this).val() === questions[4].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
 
-   });
+    });
+    $.each($("input[name='question-5']:checked"), function(){
+        if($(this).val() === questions[5].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
 
-   console.log(answers);
-   return answers;
+    });
+    $.each($("input[name='question-6']:checked"), function(){
+        if($(this).val() === questions[6].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
+
+    });
+    $.each($("input[name='question-7']:checked"), function(){
+        if($(this).val() === questions[7].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
+
+    });
+    $.each($("input[name='question-8']:checked"), function(){
+        if($(this).val() === questions[8].correctAnswer){
+            numberCorrect++
+        } else {
+            numberWrong ++;
+        };
+            
+        });
+    $.each($("input[name='question-9']:checked"), function(){
+        if($(this).val() === questions[9].correctAnswer){
+            numberCorrect ++;
+        } else {
+            numberWrong ++;
+        };
+
+        });
+    $.each($("input[name='question-10']:checked"), function(){
+        if($(this).val() === questions[10].correctAnswer){
+            numberCorrect ++;
+        } else if($(this).val() !== questions[10].correctAnswer) {
+            numberWrong ++;
+        } else if ($("input[name='question-10']:not(:checked)")) {
+            numberUnanswered ++;
+        };
+
+    });
+    $.each($("input[name='question-11']:checked"), function(){
+        if($(this).val() === questions[11].correctAnswer){
+            numberCorrect ++;
+        } else if($(this).val() == null) {
+            numberUnanswered ++;
+        } else {
+            numberWrong ++;
+        };
+        
+        
+    });
+    console.log(numberCorrect);
+    console.log(numberWrong);
+    console.log(numberUnanswered);
+    showResults();
 }
+
 
 function countDown(){
     counter --;
     $("#timer").text("Time remaining: " + counter);
-    console.log("COUNTER: "+ counter)
+    
     if (counter == 0) {
         clearInterval (timer);
     }
@@ -117,22 +211,37 @@ function startGame() {
 $("#start").on("click", startGame);
 $("#submit").on("click", getAnswers);
 
+var hideContainer = null;
+var restoreContainer = null;
+
+function showResults() {
+    hideContainer = $("#container").detach();
+    $("#container").html(hideContainer);
+    $("#resultsBox").append("<p id='tallyHead'>Results:</p><br> <p class='tally'>Correct: " + numberCorrect + "</p><br> <p class='tally'>Incorrect: " + numberWrong + "</p> <br><br> <button id='resetGame'>Play Again!</button>");
+}
+
+function resetGame() {
+    var counter = 120;
+    var numberCorrect = 0;
+    var numberWrong = 0;
+    var numberUnanswered = 0;
+    document.getElementById("gameBody").style.visibility = "hidden";
+    restoreContainer = $("#container").attach();
+    $("#container").html(restoreContainer);
+}
+
+
+$("#resetGame").on("click", resetGame);
 
 
 
-//get user input-- :checked in jQuery?
 
-//check for right or wrong or incomplete-- $.each() in jQuery - check the user values using the name property?
+//How to check for unanswered?  Two solutions not working.
 
-//record each type
-
-//display a count of each type
-
-//compile and display results when the timer runs out or when submit button is clicked
+//Start over when Play Again! is clicked.
 
 //line breaks in Question 7-- how to get \n into the JS?  Easier to do all of them on separate lines?
 
 //How to style buttons, no CSS will work except left margin
-
 
 
